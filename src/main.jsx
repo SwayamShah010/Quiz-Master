85fb8b8d05448a4372d5ce90a793c57ad085e962
SwayamShah010 <shahswayam010@gmail.com>
2026-08-31 11:25:08 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import QuizMaster from "./QuizMaster.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QuizMaster />
-  </React.StrictMode>
-);
